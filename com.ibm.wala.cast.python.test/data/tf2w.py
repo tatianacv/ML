2d11195cbeb534ab47e20cd06c5e6cfdc7efe86e
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2023-06-16 15:00:33 -0400

    Add assignment to data file

--- com.ibm.wala.cast.python.test/data/tf2w.py
+++ com.ibm.wala.cast.python.test/data/tf2w.py
@@ -7,4 +7,4 @@ def add(a, b):
 nested_value_rowids = [tf.constant([0, 0, 1, 3, 3], tf.int64),tf.constant([0, 0, 2, 2, 2, 3, 4], tf.int64)]
 x = tf.keras.layers.Input(shape=[None], dtype=tf.string)
 y = tf.RaggedTensor.from_nested_value_rowids(x, nested_value_rowids)
-add(y,y)
+c = add(y,y)
