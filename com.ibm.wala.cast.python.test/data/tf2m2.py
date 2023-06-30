4c2aba1b2439a7607eb36a97d0ed594fe00b1d41
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2023-06-30 16:29:37 -0400

    Fixing new lines in test inputs

--- com.ibm.wala.cast.python.test/data/tf2m2.py
+++ com.ibm.wala.cast.python.test/data/tf2m2.py
@@ -1,7 +1,8 @@
+import tensorflow
 from tensorflow.python.ops.math_ops import range
 
 def add(a, b):
   return a + b
 
 
-c = add(range(3, 18, 3), range(5))
+c = add(tensorflow.range(3, 18, 3), range(5))
